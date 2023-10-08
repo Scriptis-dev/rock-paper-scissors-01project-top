78266da4e02f056df8fe4f3d949028f4160cd82e
Scriptis-dev <133271683+Scriptis-dev@users.noreply.github.com>
2023-10-09 00:18:18 +0200

    Round Displaying Done

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ const choiceBtns = document.querySelectorAll('.choice-btn');
 let playerSelection;
 let computerSelection;
 let roundResult;
+
+
+choiceBtns.forEach(button => button.addEventListener('click', () => {
+
+  playerSelection = button.textContent;
+  computerTurn();
+  playerChoiceDisplay.textContent = `Player: ${playerSelection}`;
+  computerChoiceDisplay.textContent = `Computer: ${computerTurn()}`;
+  roundResultDisplay.textContent = roundWinner();
+}));
+
+
+function computerTurn() {
+  const computerSelection = ["rock", "paper", "scissors"];
+  return computerSelection[
+    Math.floor(Math.random() * computerSelection.length)
+  ].toUpperCase();
+};
+
+
+function roundWinner() {
+  if (computerSelection === "rock") {
+    return roundResult === "paper" ? 'You win!' : 'You lose!';
+  }
+  else if (computerSelection === "paper") {
+    return roundResult === 'scissors' ? 'You win!' : 'You lose!';
+  }
+  else if (computerSelection === 'scissors') {
+    return roundResult === 'rock' ? 'You win!' : 'You lose!';
+  }
+
+};
